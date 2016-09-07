88b60f18bb8ffa29c23152b47bcdc83b56bb5325
yONg <ivere27@gmail.com>
2016-09-08 00:08:40 +0900

    refactoring. use onResponse and onError callbacks

--- example.cpp
+++ example.cpp
@@ -1,108 +1,141 @@
 #include <iostream>
 #include "SimpleHttpRequest.h"
 
+const string HOSTNAME = "127.0.0.1";
+const string PORT = "10509";
+const string PATH = "/archive";
+
 using namespace std;
+using namespace request;
 
 int main() {
-  int r;
-
-  uv_loop = uv_default_loop();
-
 
 #if 1
   // request.get(url)
-  SimpleHttpRequest request(uv_loop);
+  SimpleHttpRequest request;
   request.timeout = 1000;
-  request.get("http://127.0.0.1:10509/archive")
-  .on("error", [](){
-    cerr << endl << "on error" << endl;
-  }).on("response", [&request](){
-    cout << request.responseBody.str();
+  request.get("http://" + HOSTNAME + ":" + PORT + PATH)
+  .on("error", [](Error&& err){
+    cerr << err.name << endl << err.message << endl;
+  }).on("response", [](Response&& res){
+    cout << res.str();
   }).end();
+
+  return 0;
 #endif
 
 #if 0
+  uv_loop = uv_default_loop();
+
+  // request.get(url)
+  SimpleHttpRequest request(uv_loop);
+  request.timeout = 1000;
+  request.get("http://" + HOSTNAME + ":" + PORT + PATH)
+  .on("error", [](Error&& err){
+    cerr << err.name << endl << err.message << endl;
+  }).on("response", [](Response&& res){
+    cout << res.str();
+  }).end();
+
+  return uv_run(uv_loop, UV_RUN_DEFAULT);
+#endif
+
+#if 0
+  uv_loop = uv_default_loop();
+
   // request(options) GET
-  map<string, string> options;
-  options["hostname"] = "192.168.10.9";
-  options["port"] = "10509";
-  options["path"] = "/archive";
-  options["method"] = "GET";
+  map<string, string> options = {
+    { "hostname", HOSTNAME },
+    { "port"    , PORT     },
+    { "path"    , PATH     },
+    { "method"  , "GET"    }
+  };
 
   SimpleHttpRequest request(options, uv_loop);
   request.setHeader("content-type","application/json")
-  .on("error", [](){
-    cerr << endl << "on error" << endl;
-  }).on("response", [&request](){
-    for (const auto &kv : request.responseHeaders)
+  .on("error", [](Error&& err){
+    cerr << err.name << endl << err.message << endl;
+  }).on("response", [](Response&& res){
+    for (const auto &kv : res.headers)
       cout << kv.first << " : " << kv.second << endl;
 
-    cout << request.responseBody.str().c_str();
+    cout << res.str();
   }).end();
+
+  return uv_run(uv_loop, UV_RUN_DEFAULT);
 #endif
 
 #if 0
+  uv_loop = uv_default_loop();
+
 // request.post(url, body) POST
   string body = "{\"archive\":3}";
   SimpleHttpRequest request(uv_loop);
   request.setHeader("content-type","application/json")
-  .post("http://192.168.10.9:10509/archive", body)
-  .on("error", [](){
-    cout << endl << "on error" << endl;
-  })
-  .on("response", [&request](){
-    cout << endl << request.statusCode << endl;
-    cout << request.responseBody.str() << endl;
+  .post("http://" + HOSTNAME + ":" + PORT + PATH, body)
+  .on("error", [](Error&& err){
+    cerr << err.name << endl << err.message << endl;
+  }).on("response", [](Response&& res){
+    cout << endl << res.statusCode << endl;
+    cout << res.str() << endl;
   })
   .end();
+
+  return uv_run(uv_loop, UV_RUN_DEFAULT);
 #endif
 
 
 #if 0
+  uv_loop = uv_default_loop();
+
 // request(options, headers) POST write(string)
   map<string, string> options;
   map<string, string> headers;
-  options["hostname"] = "192.168.10.9";
-  options["port"] = "10509";
-  options["path"] = "/archive";
+  options["hostname"] = HOSTNAME;
+  options["port"] = PORT;
+  options["path"] = PATH;
   options["method"] = "POST";
   headers["content-type"] = "application/json";
 
   SimpleHttpRequest request(options, headers, uv_loop);
-  request.on("error", [](){
-    cout << endl << "on error" << endl;
+  request.on("error", [](Error&& err){
+    cerr << err.name << endl << err.message << endl;
   });
-  request.on("response", [&request](){
-    cout << endl << request.statusCode << endl;
-    cout << request.responseBody.str() << endl;
+  request.on("response", [](Response&& res){
+    cout << endl << res.statusCode << endl;
+    cout << res.str() << endl;
   });
   request.write("{\"archive\":3}");
   request.end();
+
+  return uv_run(uv_loop, UV_RUN_DEFAULT);
 #endif
 
 #if 0
+  uv_loop = uv_default_loop();
+
 // request(options, headers) POST write(stream)
   map<string, string> options;
   map<string, string> headers;
-  options["hostname"] = "192.168.10.9";
-  options["port"] = "10509";
-  options["path"] = "/archive";
+  options["hostname"] = HOSTNAME;
+  options["port"] = PORT;
+  options["path"] = PATH;
   options["method"] = "POST";
   headers["content-type"] = "application/json"; // ignored!!
 
   stringstream body;
   body << "{\"archive\":3}";
   SimpleHttpRequest request(options, headers, uv_loop);
-  request.on("error", [](){
-    cout << endl << "on error" << endl;
+  request.on("error", [](Error&& err){
+    cerr << err.name << endl << err.message << endl;
   });
-  request.on("response", [&request](){
-    cout << endl << request.statusCode << endl;
-    cout << request.responseBody.str() << endl;
+  request.on("response", [](Response&& res){
+    cout << endl << res.statusCode << endl;
+    cout << res.str() << endl;
   });
   request.write(body);  //contnet-type = "application/octet-stream"
   request.end();
-#endif
 
   return uv_run(uv_loop, UV_RUN_DEFAULT);
+#endif
 }
